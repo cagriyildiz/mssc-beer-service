df17fda84ba495a810cd278fffe152bcb654afc3
cagriyildiz <cagriyild@gmail.com>
2021-07-21 18:24:44 +0300

    Create beer repository

---
+++ src/main/java/com/jurengis/msscbeerservice/repository/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.jurengis.msscbeerservice.repository;
+
+import com.jurengis.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
